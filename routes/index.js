15ac6dd128d31501f0c0ef2f916946306636e560
urik <#mamapapa88>
2014-08-23 12:18:08 -0300

    Added copmlaints sending

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var gisRouter = require('./gis');
+var reportsRouter = require('./reports');
 
 
 module.exports = exports = function(app) {
 
 	app.use('/api/gis', gisRouter());
-
+    app.use('/api/reports', reportsRouter());
 };
